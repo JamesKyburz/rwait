2f824199379544b3e46b9b08845fa94d4dd819b4
James Kyburz <james.kyburz@gmail.com>
2019-01-08 12:51:56 +0100

    run fix when no connection given

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
-module.exports = Rwait
-
-function Rwait (r) {
-  return function rwait (connection, options) {
-    if (!options) {
-      options = connection
-      connection = undefined
-    }
-    const { db, table, timeout, filter } = options
-    return new Promise((resolve, reject) => {
-      r
-        .db(db)
-        .table(table)
-        .changes()
-        .filter(filter)
-        .run(connection, (err, cursor) => {
-          if (err) return reject(err)
-          resolve({
-            value: new Promise((resolve, reject) => {
-              let timer
-              if (timeout) {
-                timer = setTimeout(() => reject(new Error('timeout')), timeout)
-              }
-              cursor.each((err, res) => {
-                if (err) return reject(err)
-                if (timer) clearTimeout(timer)
-                cursor.close().catch(f => f)
-                resolve(res.new_val)
-              })
-            })
+module.exports = r => (connection, options) => {
+  if (!options) {
+    options = connection
+    connection = undefined
+  }
+  const { db, table, timeout, filter } = options
+  return new Promise((resolve, reject) => {
+    const done = (err, cursor) => {
+      if (err) return reject(err)
+      resolve({
+        value: new Promise((resolve, reject) => {
+          let timer
+          if (timeout) {
+            timer = setTimeout(() => reject(new Error('timeout')), timeout)
+          }
+          cursor.each((err, res) => {
+            if (err) return reject(err)
+            if (timer) clearTimeout(timer)
+            cursor.close().catch(f => f)
+            resolve(res.new_val)
           })
         })
-        .catch(reject)
-    })
-  }
+      })
+    }
+    r
+      .db(db)
+      .table(table)
+      .changes()
+      .filter(filter)
+      .run(...(connection ? [connection, done] : [done]))
+      .catch(reject)
+  })
 }
